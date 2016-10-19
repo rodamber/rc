b001e6e5ecacf21e68442d3538173c45024fd34d
rodamber <rodrigoambernardo@gmail.com>
2016-10-19 23:21:23 +0100

    Template files for the betweeness metric

---
+++ project_1/lib/betweeness_centrality.cpp
@@ -0,0 +1,15 @@
+#include "betweeness_centrality.hpp"
+
+namespace project_1 {
+
+  template <class Graph>
+  double brandes_betweeness_centrality(const Graph& g) {
+    (void) g; return 0.0;
+  }
+
+  template <class Graph>
+  double normalized_brandes_betweeness_centrality(const Graph& g) {
+    (void) g; return 0.0;
+  }
+
+}; // namespace betweeness
---
+++ project_1/lib/betweeness_centrality.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace project_1 {
+
+  template <class Graph>
+  double brandes_betweeness_centrality(const Graph& g);
+
+  template <class Graph>
+  double normalized_brandes_betweeness_centrality(const Graph& g);
+
+};
