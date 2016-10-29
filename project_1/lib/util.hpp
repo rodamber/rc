002009d236ea9fb1b5d8ed5d06296727a242a817
rodamber <rodrigoambernardo@gmail.com>
2016-10-29 16:32:58 +0100

    Added barabasi-albert model

--- project_1/lib/util.hpp
+++ project_1/lib/util.hpp
@@ -1,18 +1,80 @@
 #pragma once
 
 #include <boost/graph/graph_traits.hpp>
+#include <boost/graph/iteration_macros.hpp>
+#include <boost/graph/properties.hpp>
 
-namespace project_1 {
+#include <random>
+
+
+namespace project_1 { namespace util {
 
   using namespace boost;
 
-  template <class Graph>
-  using vertex_descriptor = typename graph_traits<Graph>::vertex_descriptor;
+  namespace graph {
+    template <class Graph>
+    using vertex_descriptor = typename graph_traits<Graph>::vertex_descriptor;
 
-  template <class Graph>
-  inline bool undirected() {
-    using Cat = typename graph_traits<Graph>::directed_category;
-    return !boost::detail::is_directed(Cat());
-  }
+    template <class Graph>
+    inline bool undirected() {
+      using Cat = typename graph_traits<Graph>::directed_category;
+      return !boost::detail::is_directed(Cat());
+    }
 
-}; // namespace project_1
+  };
+
+  namespace random {
+
+    std::random_device r;
+    std::default_random_engine random_engine(r());
+
+    bool random_bool(double probability) {
+      return std::bernoulli_distribution(probability)(random_engine);
+    }
+
+  };
+
+  namespace print {
+
+    template <class T>
+    size_t digit_count(T number) {
+      size_t digits = 0;
+
+      if (number <= 0) {
+        digits = 1;
+      }
+      while (number) {
+        number /= 10;
+        digits++;
+      }
+      return digits;
+    }
+
+    std::string pad(size_t max, size_t n) {
+      return std::string(max - digit_count(n), ' ');
+    }
+
+    template <class Graph>
+    void print_graph_in_adjacency_list_format(const Graph& g) {
+      const auto V = num_vertices(g);
+      const auto max_num_digits = digit_count(V - 1);
+
+      const auto index = get(vertex_index, g);
+
+      BGL_FORALL_VERTICES_T(u, g, Graph) {
+        std::string pad0 = pad(max_num_digits, index[u]);
+        std::cout << pad0 << index[u] << " | ";
+
+        BGL_FORALL_ADJ_T(u, v, g, Graph) {
+          std::string pad1 = pad(max_num_digits, index[v]);
+          std::cout << pad1 << index[v] << " -> ";
+        }
+        std::cout << "." << std::endl;
+      }
+    }
+
+
+  }; // namespace print
+
+
+}; }; // namespace project_1::util
--- project_1/main.cpp
+++ project_1/main.cpp
@@ -1,6 +1,35 @@
+#include <iostream>
+
+#include <boost/graph/adjacency_list.hpp>
+
 #include <clustering.hpp>
 #include <betweeness.hpp>
+#include <barabasi_albert.hpp>
+#include <util.hpp>
 
 int main() {
+
+  using namespace boost;
+  using namespace project_1;
+
+  using std::cout;
+  using std::cin;
+  using std::endl;
+
+  using Graph = adjacency_list<vecS, vecS, undirectedS>;
+
+  int V = 0;
+
+  cout << "V = "; cin >> V; cout << endl;
+
+  Graph g;
+  auto u = add_vertex(g);
+  auto v = add_vertex(g);
+  add_edge(u, v, g);
+
+  barabasi_albert<Graph>(V - 2, g);
+
+  util::print::print_graph_in_adjacency_list_format(g);
+
   return 0;
 }
